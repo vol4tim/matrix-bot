3c4a31a96b1b3c6c80e0d00e8e46bcc053e13348
vol4tim <vol4tim@gmail.com>
2022-09-13 14:25:09 +0400

    upd start tikets

--- src/cron/openTicket.js
+++ src/cron/openTicket.js
@@ -22,9 +22,25 @@ async function isTicket(userId, start) {
 
 export const isRequireBot = true;
 
+function getStart() {
+  const currentMonth = Number(dayjs().format("M"));
+  let start = dayjs();
+  if (currentMonth === 1 || currentMonth === 2) {
+    start = start.subtract(1, "year").month(11);
+  } else if (currentMonth >= 3 && currentMonth <= 5) {
+    start = start.month(2);
+  } else if (currentMonth >= 6 && currentMonth <= 8) {
+    start = start.month(5);
+  } else if (currentMonth >= 9 && currentMonth <= 11) {
+    start = start.month(8);
+  } else if (currentMonth === 12) {
+    start = start.month(11);
+  }
+  return start.startOf("month").valueOf();
+}
+
 export default async function (bot) {
-  const start = dayjs().startOf("month").valueOf();
-  // const start = dayjs("01-07-2022", "MM-DD-YYYY").valueOf();
+  const start = getStart();
   const users = await getUsers();
   for (const user of users) {
     if (!(await isTicket(user.id, start))) {
